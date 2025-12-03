304a1a5acc8875c109968827aaa8000ee8c89c0b
sonu <linuxsonu@gmail.com>
2025-12-04 02:20:59 +0530

    updated invalidation

--- packages/container/src/index.js
+++ packages/container/src/index.js
@@ -1,2 +1,2 @@
 import("./bootstrap");
-// updated bootstrap path now that the package has been moved to /container checking configs folder
+// updated bootstrap path now that the package has been moved to /container checking configs folder d
